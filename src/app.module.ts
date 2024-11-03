bcc5a8610d0094706aebb01f2ac2d0f6b4d091f3
toastmanager <mrbonkyou@gmail.com>
2024-11-04 00:03:05 +0900

    chor: add database async injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<TypeOrmModuleOptions>('database.type', {
+          infer: true
+        }),
+        host: configService.get<string>('database.host'),
+        port: configService.get<string>('database.port'),
+        username: configService.get<string>('database.user'),
+        password: configService.get<string>('database.pass'),
+        database: configService.get<string>('database.name'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
