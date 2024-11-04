4a37633ae41312a200061f48279d732b1433240d
toastmanager <mrbonkyou@gmail.com>
2024-11-04 12:36:56 +0900

    refactor: set global prefix to /api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
 
   const config = new DocumentBuilder()
     .setTitle('API')
@@ -11,7 +13,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, documentFactory);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
